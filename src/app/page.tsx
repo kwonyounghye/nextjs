c76c99d999ae77e0250420b6f22d90a270febc41
kwonyounghye <kyh90721@gmail.com>
2024-10-28 17:39:40 +0900

    Design: add title to navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+        <nav>영혜의 포트폴리오</nav>
       </main>
       <footer className={styles.footer}>
        
