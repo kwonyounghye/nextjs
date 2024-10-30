b19bb13e1cf2713d1d7a4e42b1aa67fe183b1608
kwonyounghye <kyh90721@gmail.com>
2024-10-30 19:56:50 +0900

    🎨 Feat: Implement variable declaration and conditional logic

--- src/app/todoList.tsx
+++ src/app/todoList.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 
 
 export default function todoList() {
-   
+    // [상태변경 전, 후]
+    const [todo, setTodo] = useState<string>(''); // 입력값
+                                // <문자열이 배열>(빈배열로 초기화)
+    const [todos, setTodos] = useState<string[]>([]); // 출력값
+    
 
     // 추가
     const addTodo = () => {
-        
+        if(todo.trim()=='') {
+            // setTodos([...todos, setTodo]);
+        }
     }
 
     // 제거
