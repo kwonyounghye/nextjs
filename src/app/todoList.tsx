51697731ad3c1a1f932f72e18e340b49786d09e0
kwonyounghye <kyh90721@gmail.com>
2024-10-30 19:44:57 +0900

    ✨ Feat: basic structure for todoList

---
+++ src/app/todoList.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+export default function todoList() {
+   
+
+    // 추가
+    const addTodo = () => {
+        
+    }
+
+    // 제거
+    const deleteTodo = () => {
+
+    }
+    // 편집
+    const editTodo = () => {
+
+    }
+}
