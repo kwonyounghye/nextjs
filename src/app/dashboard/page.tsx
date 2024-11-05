fb969ceb81d16e16557ad330ae78fd5607df1c23
kwonyounghye <kyh90721@gmail.com>
2024-11-05 15:49:47 +0900

    🐛 fix: Resolve page navigation error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,44 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
-import TodoList from "./todoList";
 import { useState } from 'react'
-import Link from 'next/link';
 
 
-export default function Home() {
+export default function TodoList() {
+    // [상태변경 전, 후]
+    const [todo, setTodo] = useState<string>(''); // 입력값
+    // <문자열이 배열>(빈배열로 초기화)
+    const [todos, setTodos] = useState<string[]>([]); // 출력값
 
+    // 추가
+    const addTodo = () => {
+        // trim(): 앞뒤 공백 제거
+        if (todo.trim() !== '') return;
+        setTodos([...todos, todo])
+        setTodo('') // 입력필드 초기화
+    }
 
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <nav>
-          <h2>영혜의 포트폴리오</h2>
-          <ul>
-            <li><Link href="">Home</Link></li>
-            <li><Link href="/todoList">TodoList</Link></li> {/* 버튼으로 변경 */}
-          </ul>
-        </nav>
-      </main>
-    </div>
-  );
-}
+    // 제거
+    // const deleteTodo = () => {
+
+    // }
+    // 편집
+    // const editTodo = () => {
+
+    // }
+
+    return (
+        <div>
+            <div>
+                <h1>To-do List</h1>
+                <hr />
+                <input type='text' placeholder='To-do input' value={todo}
+                    onChange={(e) => setTodo(e.target.value)} />
+                <button>Add</button>
+            </div>
+            <div>
+                <span></span>
+                <button>edit</button>
+                <button>delete</button>
+            </div>
+        </div>
+    )
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <h2>영혜의 포트폴리오</h2>
           <ul>
             <li><Link href="">Home</Link></li>
-            <li><Link href="/todoList">TodoList</Link></li> {/* 버튼으로 변경 */}
+            <li><Link href="/dashboard">TodoList</Link></li> {/* 버튼으로 변경 */}
           </ul>
         </nav>
       </main>
